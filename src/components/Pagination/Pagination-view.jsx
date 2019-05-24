0e2e7130785723b944d4d27484d92ffa83be0743
Kamil Wozniak <kamilw.wozniak@gmail.com>
2019-05-24 10:34:55 +0200

    Disable links to first/last when on first/last page.

--- src/components/Pagination/Pagination-view.jsx
+++ src/components/Pagination/Pagination-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import {
+  Pagination, PaginationItem, PaginationLink, Row, Col,
+} from 'reactstrap';
 
 export default function PaginationView(props) {
   const {
@@ -13,30 +15,34 @@ export default function PaginationView(props) {
   } = props;
   if (numberOfPages > 1) {
     return (
-      <Pagination>
-        <PaginationItem>
-          <PaginationLink first onClick={() => handlePageChange(1, itemsOnPage, lastSearch)} />
-        </PaginationItem>
-        <PaginationItem disabled={Number(currentPage) === 1}>
-          <PaginationLink
-            previous
-            onClick={() => handlePageChange(currentPage - 1, itemsOnPage, lastSearch)}
-          />
-        </PaginationItem>
-        {handlePages(numberOfPages)}
-        <PaginationItem disabled={currentPage === numberOfPages}>
-          <PaginationLink
-            next
-            onClick={() => handlePageChange(currentPage + 1, itemsOnPage, lastSearch)}
-          />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink
-            last
-            onClick={() => handlePageChange(numberOfPages, itemsOnPage, lastSearch)}
-          />
-        </PaginationItem>
-      </Pagination>
+      <Row>
+        <Col>
+          <Pagination>
+            <PaginationItem disabled={Number(currentPage) === 1}>
+              <PaginationLink first onClick={() => handlePageChange(1, itemsOnPage, lastSearch)} />
+            </PaginationItem>
+            <PaginationItem disabled={Number(currentPage) === 1}>
+              <PaginationLink
+                previous
+                onClick={() => handlePageChange(currentPage - 1, itemsOnPage, lastSearch)}
+              />
+            </PaginationItem>
+            {handlePages(numberOfPages)}
+            <PaginationItem disabled={currentPage === numberOfPages}>
+              <PaginationLink
+                next
+                onClick={() => handlePageChange(currentPage + 1, itemsOnPage, lastSearch)}
+              />
+            </PaginationItem>
+            <PaginationItem disabled={currentPage === numberOfPages}>
+              <PaginationLink
+                last
+                onClick={() => handlePageChange(numberOfPages, itemsOnPage, lastSearch)}
+              />
+            </PaginationItem>
+          </Pagination>
+        </Col>
+      </Row>
     );
   }
   return null;
