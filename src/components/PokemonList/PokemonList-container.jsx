8e4d223ae4edb009a0f5e29c59e9508488deebed
Kamil Wozniak <kamilw.wozniak@gmail.com>
2019-05-27 21:36:47 +0200

    Fix no-result page horizontal alignment

--- src/components/PokemonList/PokemonList-container.jsx
+++ src/components/PokemonList/PokemonList-container.jsx
@@ -44,11 +44,14 @@ export default class PokemonListContainer extends Component {
             <PokemonCard key={pokemon.id} pokemonInfo={pokemon} handleClick={handleClick} />
           ))
         ) : (
-          <section>
+          <section className="w-100">
+            <Row>
             <Col className="text-center">
               <img className="no-result mt-3 mb-5" src={noResultImg} alt="no result" />
               <h3 className="mt-3 mb-5">Psyduck says that there is no such pokemon!</h3>
             </Col>
+            </Row>
+            
           </section>
         )}
       </Row>
